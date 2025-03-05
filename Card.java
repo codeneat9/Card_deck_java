d1f51ef8cb28bb33b1e28531e1270a1108a91bf9
codeneat9 <yashwithbehara18@gmail.com>
2025-03-05 19:01:20 +0530

    method for comparing two cards

--- Card.java
+++ Card.java
@@ -12,3 +12,11 @@ class Card {
     public String getRank() {
        return this.rank;
     }
+    public boolean sameRank(Card var1) {
+       return this.rank.equals(var1.rank);
+    }
+ 
+    public boolean sameSuit(Card var1) {
+       return this.suit.equals(var1.suit);
+    }
+ 
