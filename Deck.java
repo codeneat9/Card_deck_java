1e5213b4aac7d0c944f36eff6290e7323ec6d7b2
codeneat9 <yashwithbehara18@gmail.com>
2025-03-05 18:31:42 +0530

    Method to create a deck

--- Deck.java
+++ Deck.java
@@ -8,3 +8,13 @@ class Deck {
         deck = new ArrayList<>();
         random = new Random();  
     }
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"};
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
