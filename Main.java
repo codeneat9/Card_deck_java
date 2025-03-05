60f21530e76b71aab74c5331538ffe02bca93024
codeneat9 <yashwithbehara18@gmail.com>
2025-03-05 18:27:52 +0530

    Displaying the menu and taking input

--- Main.java
+++ Main.java
@@ -6,3 +6,17 @@ public class Main {
         Deck deck = new Deck();
         deck.createDeck();
         int choice;
+        do {
+            // Display Menu
+            System.out.println("\nCard Deck :");
+            System.out.println("1. Show Deck");
+            System.out.println("2. Find a Card");
+            System.out.println("3. Give 5 Random Cards");
+            System.out.println("4. Shuffle the Deck");
+            System.out.println("5. Compare Two Cards (Same Rank)");
+            System.out.println("6. Check if Two Cards Have Same Suit");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // Consume newline
